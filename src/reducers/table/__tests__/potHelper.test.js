251eda346c997997f845c816ff73338fb2dea150
Enkh-Amgalan Ganbaatar <enkh.ganbaatar@gmail.com>
2020-06-24 09:34:49 +0200

    refactor: calculate pot logic

---
+++ src/reducers/table/__tests__/potHelper.test.js
@@ -0,0 +1,174 @@
+const { TestScheduler } = require('jest');
+const { calculatePot } = require('../potHelper');
+
+describe('potHelper functions', () => {
+  describe('test update pot', () => {
+    test('simple case without side pot', () => {
+      const table = {
+        id: 0,
+        pot: {
+          current: 0,
+        },
+        seats: [
+          {
+            position: 0,
+            bet: 200,
+            inHand: true,
+            chipsInPlay: 200,
+          },
+          {
+            position: 0,
+            bet: 50,
+            inHand: false,
+            chipsInPlay: 120,
+          },
+          {
+            position: 0,
+            bet: 200,
+            inHand: true,
+            chipsInPlay: 50,
+          },
+        ],
+      };
+      expect(calculatePot(table)).toEqual({
+        current: 450,
+      });
+    });
+    test('one player all in, create main pot', () => {
+      const table = {
+        id: 0,
+        pot: {
+          current: 0,
+        },
+        seats: [
+          {
+            position: 0,
+            bet: 200,
+            inHand: true,
+            chipsInPlay: 0,
+          },
+          null,
+          {
+            position: 2,
+            bet: 200,
+            inHand: true,
+            chipsInPlay: 120,
+          },
+          {
+            position: 3,
+            bet: 30,
+            inHand: false,
+            chipsInPlay: 50,
+          },
+        ],
+      };
+      expect(calculatePot(table)).toEqual({
+        0: 430,
+        current: 0,
+      });
+    });
+    test('multiple player goes all in, create several side pots', () => {
+      const table = {
+        id: 0,
+        pot: {
+          current: 0,
+        },
+        seats: [
+          {
+            position: 0,
+            bet: 200,
+            inHand: true,
+            chipsInPlay: 0,
+          },
+          null,
+          {
+            position: 2,
+            bet: 300,
+            inHand: true,
+            chipsInPlay: 0,
+          },
+          {
+            position: 3,
+            bet: 300,
+            inHand: true,
+            chipsInPlay: 70,
+          },
+        ],
+      };
+      expect(calculatePot(table)).toEqual({
+        '0': 600,
+        '2': 200,
+        current: 0,
+      });
+    });
+    test('all in two successive rounds', () => {
+      const table = {
+        id: 0,
+        pot: {
+          0: 240,
+          current: 70,
+        },
+        seats: [
+          {
+            position: 0,
+            bet: 0,
+            inHand: true,
+            chipsInPlay: 0,
+          },
+          null,
+          {
+            position: 2,
+            bet: 80,
+            inHand: true,
+            chipsInPlay: 0,
+          },
+          {
+            position: 3,
+            bet: 80,
+            inHand: true,
+            chipsInPlay: 70,
+          },
+        ],
+      };
+      expect(calculatePot(table)).toEqual({
+        '0': 240,
+        '2': 230,
+        current: 0,
+      });
+    });
+    test('all in and normal pot between other players', () => {
+      const table = {
+        id: 0,
+        pot: {
+          0: 240,
+          current: 70,
+        },
+        seats: [
+          {
+            position: 0,
+            bet: 0,
+            inHand: true,
+            chipsInPlay: 0,
+          },
+          null,
+          {
+            position: 2,
+            bet: 50,
+            inHand: true,
+            chipsInPlay: 200,
+          },
+          {
+            position: 3,
+            bet: 50,
+            inHand: true,
+            chipsInPlay: 70,
+          },
+        ],
+      };
+      expect(calculatePot(table)).toEqual({
+        '0': 240,
+        current: 170,
+      });
+    });
+  });
+});
---
+++ src/reducers/table/potHelper.js
@@ -0,0 +1,48 @@
+const { filter, orderBy, sumBy } = require('lodash');
+
+const calculatePot = (table) => {
+  const pot = table.pot || { current: 0 };
+  const orderedSeatsWithBet = orderBy(
+    filter(
+      table.seats,
+      (seat) => seat && seat.sittingOut !== true && seat.bet > 0
+    ),
+    ['bet', 'chipsInPlay'],
+    ['asc']
+  );
+
+  let sumBets = sumBy(orderedSeatsWithBet, 'bet');
+
+  if (!orderedSeatsWithBet.find((seat) => seat.chipsInPlay === 0)) {
+    pot.current = pot.current + sumBets;
+    return pot;
+  }
+
+  let bets = orderedSeatsWithBet.map(({ bet }) => bet);
+  let remainingPot = sumBets;
+  // extract all in pots
+  for (let i = 0; i < orderedSeatsWithBet.length; i++) {
+    const seat = orderedSeatsWithBet[i];
+    const seatBet = bets[i];
+    if (seat.chipsInPlay > 0) {
+      continue;
+    }
+    if (seatBet > 0) {
+      pot[seat.position] = bets.reduce((acc, value) => {
+        return acc + (value >= seatBet ? seatBet : value);
+      }, 0);
+      bets = bets.map((bet) => (bet > seatBet ? bet - seatBet : 0));
+      remainingPot -= pot[seat.position];
+    }
+    // current pot should be added to side pot
+    if (pot.current > 0) {
+      pot[seat.position] += pot.current;
+    }
+  }
+  pot.current = remainingPot;
+  return pot;
+};
+
+const adjustOverBet = (table) => {};
+
+exports.calculatePot = calculatePot;
